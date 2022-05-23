27e03781834ccd932f549f7005ce59855092bb31
jwc20 <choij2015@gmail.com>
2022-05-23 14:19:43 -0700

    added method for parsing all events using year

--- lib/event.rb
+++ lib/event.rb
@@ -1,6 +1,7 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
+require "json"
 
 class Event
   attr_accessor :name, :location, :date, :url
@@ -20,16 +21,12 @@ class Event
   end
 
   def get_years_available
-    # https://iwf.sport/results/results-by-events/results-by-events-old-bw/
-    # https://iwf.sport/results/results-by-events/
-    # old_bw = Nokogiri::HTML(open(""))
+    # Gets all years available in new bodyweight and old bodyweight events page
     years = Array.new
     urls = ["https://iwf.sport/results/results-by-events/", "https://iwf.sport/results/results-by-events/results-by-events-old-bw/"]
 
     urls.each do |url|
       events_page = Nokogiri::HTML(open(url))
-
-      # events_page.css(".results_by_events select option").attribute("value").each do |year|
       events_page.css(".results_by_events select").first.css("option").each do |year|
         # puts year.text
         years.push(year.text) unless years.include?(year.text)
@@ -38,9 +35,33 @@ class Event
     puts years
   end
 
-  def get_list_of_events(year)
-    # Nokogiri::HTML(open("https://iwf.sport/results/results-by-events/?event_year=2022"))
+  def make_events(year)
+    get_events_page(year).css("#section-scroll div div.cards a").each do |post|
+      event = Event.new
+      event.name = post.css("div.container div div.col-md-5.col-12.not__cell__767__full p span").text
+      event.location = post.css("div.container div div.col-md-3.col-4.not__cell__767 p").text.delete!("\n")
+      event.date = post.css("div.container div div:nth-child(2) p").text.delete!("\n").rstrip
+      event.url = post.attribute("href").value
+    end
+  end
+
+  def print_events(years)
+    make_events(years)
+    Event.all.each do |event|
+      if event.name
+        puts "Name: #{event.name}"
+        puts "Location: #{event.location}"
+        puts "Date: #{event.date}"
+        puts "Url #{event.url}"
+      end
+    end
+  end
+
+  def get_events_page(year)
+    # Gets all events by year of competition
+    # Use this method to search
     if year > 2018
+      #get_new_bodyweight_events(year).css("#section-scroll div div.cards a:nth-child(1) div.container div div.col-md-5.col-12.not__cell__767__full p")
       get_new_bodyweight_events(year)
     elsif year < 2018
       get_old_bodyweight_events(year)
@@ -49,21 +70,28 @@ class Event
     end
   end
 
-  def get_old_bodyweight_events(year)
-    doc = Nokogiri::HTML(open("https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_year=#{year}"))
-  end
-
   def get_new_bodyweight_events(year)
     Nokogiri::HTML(open("https://iwf.sport/results/results-by-events/?event_year=#{year}"))
   end
 
+  def get_old_bodyweight_events(year)
+    Nokogiri::HTML(open("https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_year=#{year}"))
+  end
+
   def get_2018_events
+    urls = ["https://iwf.sport/results/results-by-events/?event_years=2018", "https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_years=2018"]
+
+    urls.each do |url|
+      doc_from_2018 = Nokogiri::HTML(open(url))
+    end
   end
 
   def get_events_from_location
   end
 end
 
-# binding.pry
-
 # Event.new.get_years_available
+#
+# puts Event.new.make_events(2021).text
+Event.new
+binding.pry
