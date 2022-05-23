e7351c538d4fa4a4b484b250bd801c6cc1d450be
jwc20 <choij2015@gmail.com>
2022-05-23 09:41:59 -0700

    first commit

---
+++ lib/iwf_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "iwf_ruby/version"
+
+module IwfRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iwf_ruby/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module IwfRuby
+  VERSION = "0.1.0"
+end
