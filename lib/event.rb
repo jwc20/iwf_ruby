253699cdd62cc25ce7afe3ae00f8e5723d1d4eea
jwc20 <choij2015@gmail.com>
2022-05-23 17:15:54 -0700

    wrote methods

--- lib/event.rb
+++ lib/event.rb
@@ -1,10 +1,9 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
-require "json"
 
 class Event
-  attr_accessor :name, :location, :date, :url
+  attr_accessor :name, :location, :date, :event_id
 
   @@all = []
 
@@ -32,7 +31,6 @@ class Event
         years.push(year.text) unless years.include?(year.text)
       end
     end
-    puts years
   end
 
   def make_events(year)
@@ -41,7 +39,7 @@ class Event
       event.name = post.css("div.container div div.col-md-5.col-12.not__cell__767__full p span").text
       event.location = post.css("div.container div div.col-md-3.col-4.not__cell__767 p").text.delete!("\n")
       event.date = post.css("div.container div div:nth-child(2) p").text.delete!("\n").rstrip
-      event.url = post.attribute("href").value
+      event.event_id = post.attribute("href").value
     end
   end
 
@@ -52,7 +50,7 @@ class Event
         puts "Name: #{event.name}"
         puts "Location: #{event.location}"
         puts "Date: #{event.date}"
-        puts "Url #{event.url}"
+        puts "Event ID: #{event.event_id}"
       end
     end
   end
@@ -83,17 +81,43 @@ class Event
   end
 
   def get_2018_events
+    # FIXME:
     old_bw = Nokogiri::HTML(open("https://iwf.sport/results/results-by-events/results-by-events-old-bw/?event_year=2018")).search("#section-scroll div.results")
     new_bw = Nokogiri::HTML(open("https://iwf.sport/results/results-by-events/?event_year=2018"))
     new_bw.at("#section-scroll div.results").add_child(old_bw)
   end
 
-  def get_events_from_location
+  def get_events_from_location(location)
+    # Get events from the location
   end
+
+  def get_athletes_from_event(url)
+    # Get only name of athletes in the event
+  end
+
+  def get_athletes_information_from_event(athlete)
+    # Get information about the athlete from the event
+    # name, athlete_id, nation, born, category, bweight, group
+  end
+
+  def get_snatch_results_from_event(athlete)
+    # Get only snatch results from the event
+    # rank_s, snatch1, snatch2, snatch3, snatch
+  end
+
+  def get_clean_and_jerk_results_from_event(athlete)
+    # Get only clean and jerk results from the event
+    # rank_cj, jerk1, jerk2, jerk3, jerk
+  end
+
+  def get_total_results_from_event(athlete)
+    # Get totals (highest snatch + highest clean and jerk) from the event
+    # snatch, jerk, total
+  end
+
+  # def get_results_from_event(url)
+  #   # Get all results from the event
+  # end
 end
 
-# Event.new.get_years_available
-#
-# puts Event.new.make_events(2021).text
-Event.new
 binding.pry
